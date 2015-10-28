4fc92cb1c0a21e03b04d3ecf9b004dbadc9f6beb
Camille Anelli <camilleanelli32@gmail.com>
2015-10-28 12:36:16 +0100

    add vaidates

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,9 @@
 class Restaurant < ActiveRecord::Base
+  validates :name, presence :true
+  validates :address, presence :true
+  validates_inclusion_of :category, :in => VALID_NAMES
+  VALID_NAMES = %w(chinese italian japanese french belgian Paul)
+
+  has_many :reviews, dependent: :destroy
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,5 @@
 class Review < ActiveRecord::Base
+  belongs_to :restaurant
+  validates :rating, :inclusion => { :in => 0..5,
+    :message => " should be between 0 to 5" }
 end
