ed942d50f769570918eb1bcc2374cd0b5c6a759a
Camille Anelli <camilleanelli32@gmail.com>
2015-10-28 12:05:05 +0100

    model review

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151028105736) do
+ActiveRecord::Schema.define(version: 20151028110255) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 20151028105736) do
     t.string   "category"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "rating"
+    t.text     "content"
+    t.integer  "restaurant_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
 end
