0b1129969ee1f73df69cac4d482892e3f9217c4c
Camille Anelli <camilleanelli32@gmail.com>
2015-10-28 15:34:29 +0100

    fix controller review

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  get 'restaurants' => "restaurants#index"
+  # get 'restaurants' => "restaurants#index"
 
-  get 'restaurants/:id' => "restaurants#show", as: 'restaurant'
+  # get 'restaurants/:id' => "restaurants#show", as: 'restaurant'
 
-  get 'restaurants/new' => "restaurants#new"
+  # get 'restaurants/new' => "restaurants#new"
 
-  post 'restaurants' => "restaurants#create"
+  # post 'restaurants' => "restaurants#create"
+
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
